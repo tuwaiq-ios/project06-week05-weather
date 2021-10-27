e4fcb800b137e712aa0965d52534afbd5706e0b2
kholodalabdulrhman7 <kholodalabdulrhman7>
2021-10-27 20:53:34 +0300

    project weather

---
+++ WeatherApp copy/WeatherApp/UIcolorView.swift
@@ -0,0 +1,17 @@
+//
+//  UIcolorView.swift
+//  WeatherApp
+//
+//  Created by Kholod Sultan on 20/03/1443 AH.
+//
+import UIKit
+
+extension UIColor {
+
+    static let sunnyTop = UIColor(red:192.0/255.0, green:38.0, blue:255.0, alpha: 1.00)
+    static let sunnyBottom = UIColor(red:35.0/255.0, green:2.0/255.0, blue:2.0/255.0, alpha:1.00)
+    static let snowyTop = UIColor(red: 192.0/255.0, green: 38.0, blue:255.0 , alpha: 1.00)
+    static let snowyBottom = UIColor(red:35.0/255, green:2.0/255.0, blue:255.0, alpha:1.00)
+}
+
+
---
+++ WeatherApp copy/WeatherApp/WeatherPresent.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherPresent.swift
+//  WeatherApp
+//
+//  Created by Kholod Sultan on 20/03/1443 AH.
+//
+
+import UIKit
+
+protocol WeatherPresenter {
+    var backgroundColors: [CGColor] { get }
+}
+
+class WeatherPresenterImpl: WeatherPresenter {
+    var backgroundColors: [CGColor] {
+        return sunnyColors
+    }
+
+    private let sunnyColors: [CGColor] = {
+        return [UIColor.sunnyTop.cgColor, UIColor.sunnyBottom.cgColor]
+    }()
+
+    private let snowyColors: [CGColor] = {
+        return [UIColor.snowyTop.cgColor, UIColor.snowyBottom.cgColor]
+    }()
+    
+   
+}
+
+
