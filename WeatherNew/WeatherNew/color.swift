4c9d925f0f97a841a8c4973e0be4e28373328572
Sana Alshahrani <me-sa51@hotmail.com>
2021-10-27 21:07:23 +0300

    add all

---
+++ WeatherNew/WeatherNew/color.swift
@@ -0,0 +1,18 @@
+//
+//  color.swift
+//  WeatherNew
+//
+//  Created by Sana Alshahrani on 20/03/1443 AH.
+//
+
+import UIKit
+
+extension UIColor {
+    static let sunnyTop = UIColor(red:0.80, green:0.50, blue:0.50, alpha:2.00)
+    static let sunnyBottom = UIColor(red:0.70, green:0.30, blue:0.50, alpha:3.00)
+    static let snowyTop = UIColor(red:3, green:0.70, blue:0.70, alpha:4.00)
+    static let snowyBottom = UIColor(red:0.50, green:0.40, blue:0.40, alpha:2.00)
+}
+
+
+
---
+++ WeatherNew/WeatherNew/presenter.swift
@@ -0,0 +1,28 @@
+//
+//  presenter.swift
+//  WeatherNew
+//
+//  Created by Sana Alshahrani on 20/03/1443 AH.
+//
+
+import UIKit
+
+protocol WeatherPresenter {
+    var backgroundColors: [CGColor] { get }
+}
+
+class WeatherPresenterImpl: WeatherPresenter {
+    var backgroundColors: [CGColor] {
+        return sunnyColors
+    }
+
+    private let sunnyColors: [CGColor] = {
+        return [UIColor.sunnyTop.cgColor, UIColor.sunnyBottom.cgColor]
+    }()
+
+    private let snowyColors: [CGColor] = {
+        return [UIColor.snowyTop.cgColor, UIColor.snowyBottom.cgColor]
+    }()
+}
+
+
