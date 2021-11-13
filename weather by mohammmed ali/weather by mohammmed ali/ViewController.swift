8c17bc9e8d630222e1217f51ed809c44eaf6a3ff
saja Al-qhtani <saja-alqhtani@Mh-Mac.local>
2021-11-13 16:37:25 +0300

    last update

--- weather by mohammmed ali/weather by mohammmed ali/ViewController.swift
+++ weather by mohammmed ali/weather by mohammmed ali/ViewController.swift
@@ -3,40 +3,51 @@
 //  weather by mohammmed ali
 //
 //  Created by mohammed al-qahtani on 20/03/1443 AH.
+
+
 import UIKit
 
-struct weatherAPI: Codable{
+struct WeatherData : Codable{
+    let wind: Wind
     let name: String
     let main: Main
     let weather: Array<weatherDis>
-   
+    let clouds: All
+    
 }
-
+struct All :Codable {
+    let all: Int
+}
 struct weatherDis :Codable {
     let description: String
+    let id : Int
 }
+
 struct Main : Codable {
     let temp: Double
     let humidity: Int
 }
-
-
+struct Wind: Codable{
+    let speed: Double
+}
 
 
 class ViewController: UIViewController {
-    
+    let label = UILabel()
     let label1 = UILabel()
     let label2 = UILabel()
     let label3 = UILabel()
     let label4 = UILabel()
     let label5 = UILabel()
-    let image = UIImageView()
+    let img = UIImageView()
     
-    override func viewDidLoad(){
+    override func viewDidLoad() {
         super.viewDidLoad()
-        
+        lis()
+        //labels
         let imageview = UIImageView(image: UIImage(named: "background"))
         imageview.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(imageview)
         NSLayoutConstraint.activate([
             
@@ -45,107 +56,154 @@ class ViewController: UIViewController {
             imageview.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
             imageview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0)
         ])
+        
+        view.backgroundColor = UIColor(named: "fff")
+        //view.backgroundColor = .cyan
+        label.textAlignment = .center
+        label.text = "Mohammed"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        //        view.backgroundColor = .cyan
+        
+        label1.textAlignment = .center
+        label1.text = "Mh"
+        label1.font = UIFont.boldSystemFont(ofSize: 30)
 
         
-        view.backgroundColor = UIColor(named: "weatherColour")
+        //        view.backgroundColor = .cyan
+        label2.textAlignment = .center
+        label2.text = "xsdfghj"
+        label2.font = UIFont.boldSystemFont(ofSize: 30)
+
+        let stack = UIStackView(arrangedSubviews: [label, label1, label2])
+        stack.distribution = .fillEqually
+        stack.translatesAutoresizingMaskIntoConstraints = false
         
-        label4.textAlignment = .center
-        label4.text = "nil"
-        label4.font = UIFont.boldSystemFont(ofSize: 30)
-        label4.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(label4)
+        view.addSubview(stack)
         NSLayoutConstraint.activate([
             
-            label4.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
-            label4.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 250),
-            label4.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100)
+            stack.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
+            stack.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16),
+            stack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 80)
             
         ])
         
-        let image = UIImageView(image: UIImage(named: "hum"))
-        image.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(image)
-        NSLayoutConstraint.activate([
-            
-            image.leftAnchor.constraint(equalTo: label4.leftAnchor, constant:250 ),
-            image.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 95),
-            image.heightAnchor.constraint(equalToConstant: 50),
-            image.widthAnchor.constraint(equalToConstant: 50)
-        
-    
-           
-        ])
         
         
+        //        view.backgroundColor = .cyan
         label3.textAlignment = .center
-        label3.text = "nilll"
+        label3.text = "12222222222"
         label3.translatesAutoresizingMaskIntoConstraints = false
-        label3.font = UIFont.boldSystemFont(ofSize: 40)
-        
+        label3.font = UIFont.boldSystemFont(ofSize: 30)
+
         self.view.addSubview(label3)
         NSLayoutConstraint.activate([
             
+            //    label3.leftAnchor.constraint(equalTo: self.view.leftAnchor,constant: 16),
             label3.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
-            label3.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 400),
+            label3.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200),
         ])
         
-        
-        
-        
-        
-        
-        
-        label2.textAlignment = .center
-        label2.text = "nill"
-        label2.translatesAutoresizingMaskIntoConstraints = false
-        label2.font = UIFont.boldSystemFont(ofSize: 60)
-        
-        self.view.addSubview(label2)
+        //        view.backgroundColor = .cyan
+        label4.textAlignment = .center
+        label4.text = "7777777"
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.font = UIFont.boldSystemFont(ofSize: 22)
+
+        self.view.addSubview(label4)
         NSLayoutConstraint.activate([
             
-            label2.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 16),
-            label2.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: -16),
-            label2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
-            label2.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300),
+            label4.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            label4.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -160),
+            label4.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 250),
         ])
-        label1.textAlignment = .center
-        label1.text = "nillll"
-        label1.translatesAutoresizingMaskIntoConstraints = false
-        label1.font = UIFont.boldSystemFont(ofSize: 70)
         
-        self.view.addSubview(label1)
+        //        view.backgroundColor = .cyan
+        label5.textAlignment = .center
+        label5.text = "00000000"
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.font = UIFont.boldSystemFont(ofSize: 30)
+
+        self.view.addSubview(label5)
         NSLayoutConstraint.activate([
             
-            label1.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
-            label1.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 500),
+            label5.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            //label5.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -160),
+            label5.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300),
             
             
         ])
+    
+            img.translatesAutoresizingMaskIntoConstraints = false
+            
+            view.addSubview(img)
+            
+            NSLayoutConstraint.activate([
+                
+                img.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+                //label5.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -160),
+                img.topAnchor.constraint(equalTo: label5.topAnchor, constant: 250),
+                img.heightAnchor.constraint(equalToConstant: 150),
+                img.widthAnchor.constraint(equalToConstant: 250)
+            ])
         
-        simpleGetUrlRequest()
     }
     
-    func simpleGetUrlRequest() {
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Abha&appid=90ff1b516e38b23119e8f40f83596f6a")!
+    func lis() {
+        let url = URL(string:"https://api.openweathermap.org/data/2.5/weather?q=jeddah&appid=90ff1b516e38b23119e8f40f83596f6a")!
         
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             guard let data = data else { return }
-            print(String(data: data, encoding: .utf8))
-            let weather = try? JSONDecoder().decode(weatherAPI.self, from: data)
+            let post = try? JSONDecoder().decode(WeatherData.self, from: data)
+            print(post)
             
-            
-            DispatchQueue.main.async {
-                self.label1.text = weather?.name
-                self.label2.text = "\(Int(weather!.main.temp  - 273))"
-                self.label3.text = weather!.weather[0].description
-                self.label4.text = "\(weather!.main.humidity)"
+            DispatchQueue.main.async { [self] in
+                self.label.text = "\(post!.wind.speed)"
+                self.label5.text = post!.name
+                self.label4.text = "\( (post!.main.temp - 273))"
+                self.label3.text = post!.weather[0].description
+                self.label2.text = "\(post!.main.humidity)"
+                self.label1.text = "\(post!.clouds.all)"
                 
+                self.img.image = img.image?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+                self.img.tintColor = UIColor.white
+
                 
+                var img1 : String = "nosign"
+                
+                if (801...804).contains(post!.weather.first!.id) {
+                    img1 = "cloud.fill"
+                }
+                
+                else if 800 == post!.weather.first!.id {
+                    img1 = "sun.max.fill"
+                }
+                
+                else if (701...781).contains(post!.weather.first!.id) {
+                    img1 = "smoke.fill"
+                }
+                else if (600...622) .contains(post!.weather.first!.id) {
+                    img1 = "cloud.snow.fill"
+                }
+                else if (500...531) .contains(post!.weather.first!.id) {
+                    img1 = "cloud.rain.fill"
+                }
+                else if (300...321).contains(post!.weather.first!.id) {
+                    
+                    img1 = "cloud.drizzle.fill"
+                }
+                
+                else if (200...232).contains(post!.weather.first!.id) {
+                    img1 = "cloud.bolt.rain.fill"
+                }
+                else {
+                    img1 = "nosign"
+                }
+                
+                self.img.image = UIImage (systemName: img1)
             }
         }
-        
-        
-        task.resume()
-    }
+
+    task.resume()
+    
+}
 }
